30bbcd00c5da6a150cf8c2ca4267525f129d2cad
Bohdan Rudenko <brudenko2804@gmail.com>
2023-08-24 17:56:50 +0300

    Implement the ApplicationRunner class
    Application demo version is ready to use.

--- src/main/java/ua/javarush/encoder/ApplicationRunner.java
+++ src/main/java/ua/javarush/encoder/ApplicationRunner.java
@@ -1,8 +1,12 @@
 package ua.javarush.encoder;
 
+import ua.javarush.encoder.controller.EncoderController;
+import ua.javarush.encoder.model.impl.CaesarCipherEncoder;
+import ua.javarush.encoder.utils.FileHandler;
+
 public class ApplicationRunner {
     public static void main(String[] args) {
-
-        System.out.println("Hello from dev");
+        EncoderController encoderController = new EncoderController(new CaesarCipherEncoder(), new FileHandler());
+        encoderController.run(args);
     }
 }
