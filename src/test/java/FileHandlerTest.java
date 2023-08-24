8c3fe7d2851a75e7eb43530a97238deed02d3611
Bohdan Rudenko <brudenko2804@gmail.com>
2023-08-24 15:51:38 +0300

    Change the values of the file path fields in the FileHandlerTest class
    Add resource text files to the test/resources for more convenient testing on any PC. Remove unnecessary comments.

--- src/test/java/FileHandlerTest.java
+++ src/test/java/FileHandlerTest.java
@@ -5,8 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FileHandlerTest {
-    private static final String TEST_INPUT_FILE_PATH = "C:\\Users\\USER\\Documents\\test.txt";
-    private static final String TEST_OUTPUT_FILE_PATH = "C:\\Users\\USER\\Documents\\test_copy.txt";
+    private static final String TEST_INPUT_FILE_PATH = "src/test/resources/files_for_FileHandler_tests/FileHandler_test.txt";
+    private static final String TEST_OUTPUT_FILE_PATH = "src/test/resources/files_for_FileHandler_tests/FileHandler_test_copy.txt";
     private static final String TEST_FILE_CONTENT = """
             Some file\tcontent
             for testing
@@ -16,19 +16,14 @@ class FileHandlerTest {
     void testReadWriteData() {
         FileHandler fileHandler = new FileHandler();
 
-        // Read data from the file
         String firstReadContent = assertDoesNotThrow(() -> fileHandler.readData(TEST_INPUT_FILE_PATH));
 
-        // Check for validness
         assertEquals(TEST_FILE_CONTENT, firstReadContent);
 
-        // Write data again to the file
         assertDoesNotThrow(() -> fileHandler.writeData(TEST_OUTPUT_FILE_PATH, firstReadContent));
 
-        // Read data from the file again
         String secondReadContent = assertDoesNotThrow(() -> fileHandler.readData(TEST_OUTPUT_FILE_PATH));
 
-        // Check if the second read content matches the original content
         assertEquals(TEST_FILE_CONTENT, secondReadContent);
     }
 }
