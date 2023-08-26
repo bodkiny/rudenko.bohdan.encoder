f91da4a9935e58a18d6d77fc39f003dca14d5207
Bohdan Rudenko <brudenko2804@gmail.com>
2023-08-26 17:09:20 +0300

    Rename the parameter of the constructor

--- src/main/java/ua/javarush/encoder/utils/InvalidArgumentsException.java
+++ src/main/java/ua/javarush/encoder/utils/InvalidArgumentsException.java
@@ -1,7 +1,7 @@
 package ua.javarush.encoder.utils;
 
 public class InvalidArgumentsException extends RuntimeException {
-    public InvalidArgumentsException(String s) {
-        super(s);
+    public InvalidArgumentsException(String message) {
+        super(message);
     }
 }
